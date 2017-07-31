a9fbe8657ac6f9a2c6119128fc888992862de120
albertzzy <zhaozi_ye_hi@126.com>
2017-07-31 19:58:03 +0800

    add test

---
+++ test/fixtures/a.js
@@ -0,0 +1,5 @@
+import { odd } from './b'
+import React from 'react';
+// export var counter = 0;
+
+odd(2)
--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
 const expect = require('expect');
+const fs = require('fs');
+const webpack = require('webpack');
+
+const config = require('./fixtures/webpack.config.js');
+
+const webpackCompiler = webpack(config);
+
+
+function isPlainObject(obj){
+	return typeof obj === 'object' && obj!==null && obj !== obj.window && Object.getPrototypeOf(obj) === Object.prototype;
+}
+
 
 describe('to log repeatedly bundled modules',function(){
 
-    it('should return a empty plain json',function(done){
+    it('should return a plain json',function(done){
         
+        webpackCompiler.run((err,stats)=>{
+            expect(err).toBe(null);
 
+            let content = fs.readFileSync('test/fixtures/res.json','utf8');
+            let isJson = isPlainObject(JSON.parse(content));
+
+            expect(isJson).toBe(true);
+
+            done();
+
+        })
 
     })
 
